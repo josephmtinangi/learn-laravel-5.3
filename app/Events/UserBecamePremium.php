3276749dbff0fb53bc1bf8c3e2cb25e4e4b6d8aa
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-20 20:49:56 +0300

    Rename the event name

--- app/Events/UserBecamePremium.php
+++ app/Events/UserBecamePremium.php
@@ -9,7 +9,7 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class UserBecomePremium
+class UserBecamePremium
 {
     use InteractsWithSockets, SerializesModels;
 
